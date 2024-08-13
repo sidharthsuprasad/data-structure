4b07f66c8b5f543da2441196aa56baeb37502c0e
sidharthsuprasad <147634466+sidharthsuprasad@users.noreply.github.com>
2024-08-13 11:16:58 +0530

    Add files via upload

---
+++ LAB/bubble.c
@@ -0,0 +1,32 @@
+//program for bubble search
+//P S SIDHARTH
+//Roll no:49
+#include<stdio.h>
+void main()
+{
+	int a[50],temp,n,i,j;
+	printf("enter the limit");
+	scanf("%d",&n);
+	printf("enter the array element");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+	{	
+		if(a[j]>a[j+1])
+		{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+	}
+	 printf("the sorted array is");
+  for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
